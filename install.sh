75f29ce0cee9eec886aaa4d37cbf48bef41a6499
Corey Russ <cruss.seq11@gmail.com>
2021-04-19 00:08:17 -0700

    change permissions

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo cp -r deepin/ /usr/share/sddm/themes/
 sudo sed -i "s/^Current=.*/Current=deepin/g" /etc/sddm.conf
+sudo chmod 755 /usr/share/sddm/themes/deepin
 echo "Theme has been installed. Enjoy it!"
